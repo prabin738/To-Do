dfab9ba26333c1e06f3cd34a2a24b1fd0cbe1d81
prabin738 <prabinsingh750@gmail.com>
2025-08-26 16:06:29 +0545

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>ToDo List - by Prabin Singh Thakuri</h1>
+      <form>
+        <input type="text" />
+        <button>Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
