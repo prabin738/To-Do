226ced7e7e7e9bb1bb92a8a9bfd438a36f574d65
prabin738 <prabinsingh750@gmail.com>
2025-08-26 19:01:37 +0545

    Design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const App = () => {
+  //usestate array to store our todo data
+  let [todoList, setTodoList] = useState([]);
+  let saveToDoList = (event) => {
+    let todoname = event.target.todoname.value;
+    if (!todoList.includes(todoname)) {
+      let finalToDolist = [...todoList, todoname];
+      setTodoList(finalToDolist);
+    } else {
+      alert("To Do Name Already Exists");
+    }
+
+    alert(todoname);
+
+    event.preventDefault();
+  };
   return (
     <div className="App">
       <h1>ToDo List - by Prabin Singh Thakuri</h1>
-      <form>
-        <input type="text" />
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="todoname" />
         <button>Save</button>
       </form>
+      <div className="outer-div">
+        <ul>
+          <li>
+            HTML <span>&times;</span>{" "}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
